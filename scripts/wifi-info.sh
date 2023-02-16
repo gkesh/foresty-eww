9a43b0c67226e474ac1571f7d53ad049b24373b0
gkesh <gkeshthapa@gmail.com>
2023-02-16 12:50:18 +0545

    Added null guard to wifi info script

--- scripts/wifi-info.sh
+++ scripts/wifi-info.sh
@@ -1,1 +1,5 @@
-nmcli dev wifi | awk '$1 ~ /\*/' | awk '{ printf "{\"ap\": \"%s\", \"strength\": \"%d\"}", $3, $8 }' | rev | cut -c 1- | rev
+if [[ $(nmcli | grep -m1 wlp0s20f3 | awk '{print $2}') == 'disconnected' ]]; then
+    echo '{"ap": "N/A", "strength": "0"}';
+else
+    nmcli dev wifi | awk '$1 ~ /\*/' | awk '{ printf "{\"ap\": \"%s\", \"strength\": \"%d\"}", $3, $8 }' | rev | cut -c 1- | rev;
+fi
