b9981b162f97d8a530d77e384781e3911179daee
gkesh <gkeshthapa@gmail.com>
2025-04-27 22:25:50 -0400

    Added performance profile script and integration

---
+++ scripts/set-performance-info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CUR_MODE=$(asusctl profile -p | tail -1f | awk '{print $4}')
+
+if [ $CUR_MODE == 'Performance' ]; then
+    eww update mode_icon="󱪈 "
+    eww update mode_text="Performance Mode"
+elif [ $CUR_MODE == 'Quiet' ]; then
+    eww update mode_icon="  "
+    eww update mode_text="Eco Mode"
+elif [ $CUR_MODE == 'Balanced' ]; then
+    eww update mode_icon="󰚀 "
+    eww update mode_text="Balanced Mode"
+fi
